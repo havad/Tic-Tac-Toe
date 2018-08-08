6edc7327876ab09a4ff18fdeadc6234667d99d75
havad <yeahprobably@gmail.com>
2018-08-08 17:33:44 -0500

    players now cannot play a square thats already been played

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var player = "x"
 
 function play(img) {
-    if (player == "x") {
-        console.log("player is " + player);
-        document.getElementById(img).src = "x.png";
-        player = "o";
-    }
-    else if (player == "o") {
-        console.log("player is " + player);
-        document.getElementById(img).src = "o.png";
-        player = "x";
+    var check = checkPlayed(img);
+    if(check == false) {
+        if (player == "x") {
+            console.log("player is " + player);
+            document.getElementById(img).src = "x.png";
+            player = "o";
+        }
+        else if (player == "o") {
+            console.log("player is " + player);
+            document.getElementById(img).src = "o.png";
+            player = "x";
+        }
     }
 }
 
@@ -24,4 +27,13 @@ function reset() {
     document.getElementById("bottomMiddleImage").src = "blank.png";
     document.getElementById("bottomRightImage").src = "blank.png";
     player = "x";
+}
+
+function checkPlayed(img) {
+    if(document.getElementById(img).src == "http://127.0.0.1:5500/Tic-Tac-Toe/blank.png") {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
