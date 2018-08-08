3b408a0fb1af1df085b22805bee79a0d69f0da6a
havad <yeahprobably@gmail.com>
2018-08-08 17:55:16 -0500

    a boardstate is saved after a play is made so that a check win function can be written later

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var player = "x"
+var player = "x";
+var boardstate = ["0","0","0","0","0","0","0","0","0"];
 
 function play(img) {
     var check = checkPlayed(img);
@@ -6,15 +7,48 @@ function play(img) {
         if (player == "x") {
             console.log("player is " + player);
             document.getElementById(img).src = "x.png";
+            editBoardstate(img);
             player = "o";
         }
         else if (player == "o") {
             console.log("player is " + player);
             document.getElementById(img).src = "o.png";
+            editBoardstate(img);
             player = "x";
         }
     }
 }
+
+function editBoardstate(img) {
+    if(img == "topLeftImage") {
+        boardstate[0] = player;
+    }
+    else if(img == "topMiddleImage") {
+        boardstate[1] = player;
+    }
+    else if(img == "topRightImage") {
+        boardstate[2] = player;
+    }
+    else if(img == "middleLeftImage") {
+        boardstate[3] = player;
+    }
+    else if(img == "middleMiddleImage") {
+        boardstate[4] = player;
+    }
+    else if(img == "middleRightImage") {
+        boardstate[5] = player;
+    }
+    else if(img == "bottomLeftImage") {
+        boardstate[6] = player;
+    }
+    else if(img == "bottomMiddleImage") {
+        boardstate[7] = player;
+    }
+    else if(img == "bottomRightImage") {
+        boardstate[8] = player;
+    }
+    console.log(boardstate);
+ }
 
 function reset() {
     document.getElementById("topLeftImage").src = "blank.png";
@@ -27,6 +61,9 @@ function reset() {
     document.getElementById("bottomMiddleImage").src = "blank.png";
     document.getElementById("bottomRightImage").src = "blank.png";
     player = "x";
+    for(var i = 0; i < 9; i++) {
+        boardstate[i] = "0";
+    }
 }
 
 function checkPlayed(img) {
